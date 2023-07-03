3d2eef6f3d25fe4209a56c03606a2e3f9a9f694c
Joynerplus <joynerplus@msn.com>
2023-07-03 16:43:43 -0500

    actualizacion
    se agrega la funcion de eliminar y agregrar tanto a pacientes como doctores, se ajusta estilos CSS en formulario

--- routes/medicos.js
+++ routes/medicos.js
@@ -3,20 +3,17 @@ var router = express.Router();
 const { connection } = require('../database/conexion.js')
 
 router.get('/', function (req, res, next) {
-  connection.query('SELECT * FROM medicos', (error, results) => {
+   connection.query('SELECT * FROM medicos', (error, results) => {
     if (error) {
-      console.log("Error en la consulta", error)
-      res.status(500).send("Error en la consulta")
+        console.log("Error en la consulta", error)
+        res.status(500).send("Error en la consulta")
     } else {
-      res.render('medicos', {
+        res.render('medicos', {         
         title: 'Medicos',
-        subtitulo: 'LISTADO DE MEDICOS',
-        botonAgregar: 'agregar medicos', medicos: results
-      })
+        subtitulo: 'LISTADO DE MEDICOS',  medicos: results })
     }
-  });
 });
-
+});
 
 router.get('/agregar-medico', function (req, res, next) {
   connection.query('SELECT especialidad FROM medicos', (error, results) => {
@@ -24,7 +21,6 @@ router.get('/agregar-medico', function (req, res, next) {
       console.log("Error en la consulta", error)
       res.status(500).send("Error en la consulta")
     } else {
-
       let especialidades = ['Medicina general', 'Cardiología', 'Medicina interna', 'Dermatología', 'Rehabilitación física', 'Psicología', 'Odontología', 'Radiología']
       let resultsEspecialidades = results.map(objeto => objeto.especialidad);//separar packete 
       let resultsSinRepetidos = especialidades.filter((elemento) => {//filtrar repetidos
@@ -35,6 +31,7 @@ router.get('/agregar-medico', function (req, res, next) {
   });
 });
 
+
 //Agregar medicos
 router.post('/agregar', (req, res) => {
   const cedula = req.body.cedula;
@@ -44,12 +41,12 @@ router.post('/agregar', (req, res) => {
   const consultorio = req.body.consultorio;
   const especialidad = req.body.especialidad;
   connection.query(`INSERT INTO medicos (cedula, nombre, apellido, especialidad,consultorio, correo) VALUES (${cedula},'${nombre}', '${apellido}', '${especialidad}', '${consultorio}', '${correo}')`, (error, result) => {
-    if (error) {
-      console.log("Ocurrio un error en la ejecución", error)
-      res.status(500).send("Error en la consulta");
-    } else {
-      res.redirect('/medicos');
-    }
+      if (error) {
+          console.log("Ocurrio un error en la ejecución", error)
+          res.status(500).send("Error en la consulta");
+      } else {
+          res.redirect('/medicos');
+      }
   });
 })
 
@@ -57,19 +54,19 @@ router.post('/agregar', (req, res) => {
 router.get('/eliminar/:cedula', function (req, res, next) {
   const cedula = req.params.cedula
   connection.query(`DELETE FROM cita_medica WHERE id_medico=${cedula}`, (error, results) => {
-    if (error) {
-      console.log("Error en la consulta", error)
-      res.status(500).send("Error en la consulta")
-    } else {
-      connection.query(`DELETE FROM medicos WHERE cedula=${cedula}`, (error, results) => {
-        if (error) {
+      if (error) {
           console.log("Error en la consulta", error)
           res.status(500).send("Error en la consulta")
-        } else {
-          res.redirect('/medicos')
-        }
-      });
-    }
+      } else {
+          connection.query(`DELETE FROM medicos WHERE cedula=${cedula}`, (error, results) => {
+              if (error) {
+                  console.log("Error en la consulta", error)
+                  res.status(500).send("Error en la consulta")
+              } else {
+                  res.redirect('/medicos')
+              }
+          });
+      }
   });
 });
 
--- routes/pacientes.js
+++ routes/pacientes.js
@@ -2,10 +2,69 @@ var express = require('express');
 var router = express.Router();
 const { connection } = require('../database/conexion.js')
 
-router.get('/', function(req, res, next) {
-    res.render('pacientes', { 
-    title: 'Pacientes',  
-    botonAgregar: 'agregar pacientes'        
+
+router.get('/', function (req, res, next) {
+    connection.query('SELECT * FROM pacientes', (error, results) => {
+        if (error) {
+            console.log("Error en la consulta", error)
+            res.status(500).send("Error en la consulta")
+        } else {
+            res.render('pacientes', {
+                title: 'Pacientes',
+                subtitulo: 'LISTADO DE PACIENTES', medicos: results,
+                pacientes: results, opcion:
+                    'disabled', estado: true
+            })
+
+        }
     });
-  });
+});
+
+
+router.get('/agregar-paciente', function (req, res, next) {
+    res.render('registro-pacientes');
+});
+
+//Agregar paciente
+router.post('/agregar', (req, res) => {
+    const cedulaPaciente = req.body.cedulaPaciente
+    const nombrePaciente = req.body.nombrePaciente
+    const apellidoPaciente =  req.body.apellidoPaciente
+    const edadPaciente = req.body.edadPaciente
+    const telefonoPaciente = req.body.telefonoPaciente
+    connection.query(`INSERT INTO pacientes (cedula,nombre,apellido,edad,telefono) VALUES (${cedulaPaciente},'${nombrePaciente}','${apellidoPaciente}',${edadPaciente},${telefonoPaciente});`, (error, results) => {
+        if (error) {
+            console.log("Error en la consulta", error)
+            res.status(500).send("Error en la consulta")
+        } else {
+            res.redirect('/pacientes')
+        }
+    });    
+})
+
+//Eliminar Pacientes
+router.get('/eliminar/:cedula', function (req, res, next) {
+    const cedula = req.params.cedula
+    connection.query(`DELETE FROM cita_medica WHERE id_paciente=${cedula}`, (error, results) => {
+        if (error) {
+            console.log("Error en la consulta", error)
+            res.status(500).send("Error en la consulta")
+        } else {
+            connection.query(`DELETE FROM pacientes WHERE cedula=${cedula}`, (error, results) => {
+                if (error) {
+                    console.log("Error en la consulta", error)
+                    res.status(500).send("Error en la consulta")
+                } else {
+                    res.redirect('/pacientes')
+                }
+            });
+        }
+    });
+});
+
+
+
+
+
+
 module.exports = router;
